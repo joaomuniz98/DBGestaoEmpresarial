caa7b2232de08ddb5da9ff695d963a14b5f847e4
João Vitor Alves Muniz <joao.vitor@akamaetech.com>
2024-09-28 13:54:46 -0300

    Add project files.

---
+++ DBGestaoEmpresarial.WebUI.Server/Program.cs
@@ -0,0 +1,30 @@
+using DBGestaoEmpresarial.WebUI.Server.Components;
+using DBGestaoEmpresarial.Application;
+using DBGestaoEmpresarial.Infrastructure;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>();
+
+app.Run();
